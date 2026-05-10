38c8cc6e2fa04e928dc708a518ffaad843611722
Apipilikas <apipilikas@gmail.com>
2026-05-10 13:01:10 +0200

    Changed to vfl-train requestType

--- python/vfl-train-model/main.py
+++ python/vfl-train-model/main.py
@@ -97,8 +97,7 @@ class ServerModel(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.fc(x)
         return self.sigmoid(x)
 
 
