e0c5270bb25ca2a480f94fd348768d2efab13b51
Apipilikas <apipilikas@gmail.com>
2026-05-12 11:12:22 +0200

    Added imagePullPolicy in deploy_job to resolve bugs

--- go/cmd/agent/deploy_job.go
+++ go/cmd/agent/deploy_job.go
@@ -159,7 +159,7 @@ func deployJob(ctx context.Context, msChain []mschain.MicroserviceMetadata, jobN
 		container := v1.Container{
 			Name:            microservice.Name,
 			Image:           fullImage,
-			ImagePullPolicy: "IfNotPresent",
+			ImagePullPolicy: "Always",
 			Env: []v1.EnvVar{
 				{Name: "DATA_STEWARD_NAME", Value: strings.ToUpper(dataStewardName)},
 				{Name: "DESIGNATED_GRPC_PORT", Value: strconv.Itoa(port)},
@@ -213,7 +213,7 @@ func addSidecar() v1.Container {
 	return v1.Container{
 		Name:            sidecarName,
 		Image:           fullImage,
-		ImagePullPolicy: "IfNotPresent",
+		ImagePullPolicy: "Always",
 		Env: []v1.EnvVar{
 			{Name: "DESIGNATED_GRPC_PORT", Value: strconv.Itoa(firstPortMicroservice - 1)},
 			{Name: "TEMPORARY_JOB", Value: "true"},
