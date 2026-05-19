755d4c40051059419d38ac21fa9571d2f599c0c0
Apipilikas <apipilikas@gmail.com>
2026-05-19 17:15:22 +0300

    Added jaeger port forwarding

--- scripts/forward_ports.sh
+++ scripts/forward_ports.sh
@@ -6,6 +6,7 @@ kubectl port-forward svc/prometheus-kube-prometheus-prometheus 9090:9090 -n moni
 kubectl port-forward svc/api-gateway 8080:8080 -n api-gateway &
 kubectl port-forward svc/orchestrator 8090:8080 -n orchestrator &
 kubectl port-forward svc/prometheus-grafana 3000:80 -n monitoring &
+kubectl port-forward service/jaeger 16686:16686 -n linkerd-jaeger &
 
 echo -e "\nAll ports forwarded! Press Ctrl+C to exit.\n"
 wait
