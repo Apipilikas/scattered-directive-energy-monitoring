87d43028de0348a9e72732268730c762d7575398
Apipilikas <apipilikas@gmail.com>
2026-06-19 11:29:09 +0200

    Remove redundant agents

--- energy-monitoring-pipeline/execute_experiment.py
+++ energy-monitoring-pipeline/execute_experiment.py
@@ -17,7 +17,7 @@ REQUEST_APPROVAL_DATA_PROVIDERS = ["server", "clientone", "clienttwo", "clientth
 REQUEST_APPROVAL_BODY = {
     "type": "vflTrainModelRequest",
     "user": {
-      "id": "1234",
+      "id": "GUID",
       "userName": "evangelos.pipilikas@student.uva.nl"
     },
     "dataProviders": REQUEST_APPROVAL_DATA_PROVIDERS,
@@ -25,7 +25,7 @@ REQUEST_APPROVAL_BODY = {
       "type": "vflTrainModelRequest",
       "data": {
         "learning_rate": 0.1,
-        "cycles": 180,
+        "cycles": 50,
         "policy_removal": -1,
         "policy_reintroduction": -1,
         "training_backtrack": 0
@@ -109,7 +109,7 @@ def execute_experiment_run(run_no: int):
     experiment_start_time = time.time()
     
     print("Waiting for idle period ...")
-    time.sleep(conf.IDLE_PERIOD)
+    # time.sleep(conf.IDLE_PERIOD)
     
     idle_energy = _get_energy_comsumption()
     total_idle_energy = _calculate_total_energy(idle_energy)
